487e4f3a658ca52ef1f2ab39a3b19104ceb35c6e
unknown <f.barghi77@gmail.com>
2022-07-16 14:57:37 +0200

    All other types in ts

--- dist/index.js
+++ dist/index.js
@@ -4,3 +4,35 @@ let numbers = [1, 2, 3];
 let user = [1, "Fateme"];
 ;
 let mySize = 1;
+function calculateTax(income, taxYear = 2022) {
+    if (income < 50000 && taxYear > 2018)
+        return income * 0.3;
+    else
+        return income * 0.5;
+}
+let employee = {
+    id: 1,
+    name: "Fateme",
+    retire: (date) => console.log(date)
+};
+let newEmployee = {
+    id: 2,
+    name: "Ati",
+    retire: (date) => console.log(date)
+};
+function kgToLbs(weight) {
+    if (typeof (weight) === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let quantity = 100;
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
+//variable
 let age: number = 23;
+
+//array
 let numbers: number[] = [1, 2, 3]
 
 //tuple
@@ -17,4 +20,74 @@ function calculateTax(income:number, taxYear = 2022):number {
     else
         return income*0.5
 }
+
+//object
+let employee: {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+} = {
+    id: 1,
+    name: "Fateme",
+    retire: (date: Date) => console.log(date)
+}
+
+//type alias
+//we can rewrite the object in this way, it's more readable and a best practice 
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (date: Date) => void
+}
+
+let newEmployee: Employee = {
+    id: 2,
+    name: "Ati",
+    retire: (date: Date) => console.log(date)
+}
+
+//union type
+//we can give a variable more than one type 
+function kgToLbs(weight: number | string) :number {
+    // narrowing
+    if (typeof (weight) === "number")
+        return weight * 2.2;
+    else
+        return parseInt(weight) * 2.2;
+}
+
+//intersection type
+type Draggable = {
+    drag: () => void;
+}
+
+type Resizable = {
+    resize: () => void;
+}
+
+type UIWidget = Draggable & Resizable;
+
+let textBox: UIWidget = {
+    drag: () => { },
+    resize: () => { }
+}
+
+//literal types
+//sometimes we want to limite the values we can assign to a variable
+type Quantity = 50 | 100;
+let quantity: Quantity = 100;
+
+//optional chaining
+type Customer = {
+    birthday: Date;
+}
+
+function getCustomer(id: number): Customer | null{
+    return id === 0 ? null : { birthday: new Date() }
+}
+
+let customer = getCustomer(1);
+//this line of code executes only if the getCustomer() does not return null
+console.log(customer?.birthday)
+
 
